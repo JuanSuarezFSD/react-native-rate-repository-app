3b73bac52303dcc284d499462ecf2af3499a04d4
Juan Jesús Suarez <juan.suarez@hyve.net>
2023-04-12 10:38:17 +0100

    RepositoryList.jsx created

---
+++ rate-repository-app/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import {Text, View} from 'react-native'
+import Constants from 'expo-constants'
+import RepositoryList from "./RepositoryList"
+
+const Main = () => {
+   return(
+    <View style={{marginTop: Constants.statusBarHeight, flexGrow: 1, marginLeft: 3}}>
+        <RepositoryList/>
+    </View>
+   )
+   
+}
+export default Main
