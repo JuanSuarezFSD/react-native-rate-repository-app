61b2992d326c4c4338d06911d7d712832a1470bd
Juan Jesús Suarez <juan.suarez@hyve.net>
2023-05-02 10:00:09 +0100

    useRepositories.js finished

--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -1,23 +1,27 @@
-import React, { useEffect, useState } from'react'
+import React, { useEffect, useState } from "react"
+import { useQuery } from "@apollo/client"
+import { GET_REPOSITORIES } from "../graphql/queries"
 
 const useRepositories = () => {
-    const [repositories, setRepositories] = React.useState(null)
-    
-    const fetchRepositories = async () => {
-        const response = await globalThis.fetch("http://localhost:5000/api/repositories")
-        const json = await response.json()
-        setRepositories(json)
-    }
+  
+  const [repositories, setRepositories] = React.useState(null);
 
-    useEffect(() => {
-        fetchRepositories()
-    },[])
-    
-    const repositoriesNodes = repositories 
-    ? 
-    repositories.edges.map(edge => edge.node)
-    : []
-    
-    return {repositories: repositoriesNodes}
-}
-export default useRepositories
+  const fetchRepositories = async () => {
+    const response = await globalThis.fetch(
+      "http://localhost:5000/api/repositories"
+    );
+    const json = await response.json();
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  const repositoriesNodes = repositories
+    ? repositories.edges.map((edge) => edge.node)
+    : [];
+
+  return { repositories: repositoriesNodes };
+};
+export default useRepositories;
